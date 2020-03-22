4c0fe91b35cfd47516d77328de16c8f767868891
Cooper Strahan <cooperstrahan@gmail.com>
2020-03-22 14:35:39 -0600

    Part 3 Working

--- CompilersProject/src/com/company/microGrammarBaseListener.java
+++ CompilersProject/src/com/company/microGrammarBaseListener.java
@@ -5,18 +5,37 @@ import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * This class provides an empty implementation of {@link microGrammarListener},
  * which can be extended to create a listener which only needs to handle a subset
  * of the available methods.
  */
 public class microGrammarBaseListener implements microGrammarListener {
+
+    ArrayList<SymbolTable> symbolTables = new ArrayList<SymbolTable>();
+	int currentTable = 0;
+	int currentBlock = 0;
+
+	public ArrayList<SymbolTable> getSymbolTable(){
+		return symbolTables;
+	}
+
+	private void createBlock(){
+	    symbolTables.add(new SymbolTable("BLOCK", currentBlock));
+        currentTable++;
+    }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterProgram(microGrammarParser.ProgramContext ctx) { }
+	@Override public void enterProgram(microGrammarParser.ProgramContext ctx) {
+	    symbolTables.add(new SymbolTable("GLOBAL"));
+    }
 	/**
 	 * {@inheritDoc}
 	 *
@@ -64,7 +83,11 @@ public class microGrammarBaseListener implements microGrammarListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterString_decl(microGrammarParser.String_declContext ctx) { }
+	@Override public void enterString_decl(microGrammarParser.String_declContext ctx) {
+		String id = ctx.id().getText();
+		String value = ctx.str().getText();
+		symbolTables.get(currentTable).addString(id, value);
+	}
 	/**
 	 * {@inheritDoc}
 	 *
@@ -94,7 +117,20 @@ public class microGrammarBaseListener implements microGrammarListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitVar_decl(microGrammarParser.Var_declContext ctx) { }
+	@Override public void exitVar_decl(microGrammarParser.Var_declContext ctx) {
+		String type = ctx.var_type().getText();
+		String[] idList = ctx.id_list().getText().split(",");
+		for(String name : idList){
+		    if(!symbolTables.get(currentTable).duplicateDeclaration){
+                if(symbolTables.get(currentTable).variables.get(name) != null){
+                    System.out.println("DECLARATION ERROR " + name );
+                    symbolTables.get(currentTable).duplicateDeclaration = true;
+                }
+                symbolTables.get(currentTable).addString(name, type);
+            }
+		}
+
+	}
 	/**
 	 * {@inheritDoc}
 	 *
@@ -166,7 +202,11 @@ public class microGrammarBaseListener implements microGrammarListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitParam_decl(microGrammarParser.Param_declContext ctx) { }
+	@Override public void exitParam_decl(microGrammarParser.Param_declContext ctx) {
+	    String name = ctx.id().getText();
+	    String type = ctx.var_type().getText();
+	    symbolTables.get(currentTable).addVar(name, type);
+    }
 	/**
 	 * {@inheritDoc}
 	 *
@@ -196,7 +236,11 @@ public class microGrammarBaseListener implements microGrammarListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterFunc_decl(microGrammarParser.Func_declContext ctx) { }
+	@Override public void enterFunc_decl(microGrammarParser.Func_declContext ctx) {
+	    String func_name = ctx.id().getText();
+	    symbolTables.add(new SymbolTable(func_name));
+	    currentTable++;
+    }
 	/**
 	 * {@inheritDoc}
 	 *
@@ -448,7 +492,10 @@ public class microGrammarBaseListener implements microGrammarListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterIf_stmt(microGrammarParser.If_stmtContext ctx) { }
+	@Override public void enterIf_stmt(microGrammarParser.If_stmtContext ctx) {
+	    currentBlock++;
+	    createBlock();
+    }
 	/**
 	 * {@inheritDoc}
 	 *
@@ -460,7 +507,15 @@ public class microGrammarBaseListener implements microGrammarListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterElse_part(microGrammarParser.Else_partContext ctx) { }
+	@Override public void enterElse_part(microGrammarParser.Else_partContext ctx) {
+	    try{
+	        ctx.empty().getText();
+        } catch (NullPointerException np) {
+            currentBlock++;
+            createBlock();
+        }
+
+    }
 	/**
 	 * {@inheritDoc}
 	 *
@@ -496,7 +551,10 @@ public class microGrammarBaseListener implements microGrammarListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterWhile_stmt(microGrammarParser.While_stmtContext ctx) { }
+	@Override public void enterWhile_stmt(microGrammarParser.While_stmtContext ctx) {
+        currentBlock++;
+        createBlock();
+    }
 	/**
 	 * {@inheritDoc}
 	 *
